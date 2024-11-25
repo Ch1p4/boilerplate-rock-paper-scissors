df21f157b1264bbb68f26949f2753da9923ae302
Ch1p4 C <mteofilo@protonmail.com>
2024-11-25 19:53:00 +0000

    Improve the player function

--- RPS.py
+++ RPS.py
@@ -1,10 +1,52 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+from typing import Dict, List, Optional
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+def player(
+    opponent_prev_play: str,
+    opponent_play_hist: List[str] = [],
+    opponent_play_hist_combos: Dict[str, int] = {},
+    combo_length: int = 5,
+) -> str:
+    """
+    Using Markov strategy based on Abbey's,
+    with several improvents, mainly the combo lenght.
+    It predicts the opponent's next move and plays the ideal counter move.
+    """
+    
+    opponent_play_hist.append(opponent_prev_play)
+       
+    if len(opponent_play_hist) >= combo_length:
+        # Update play history combos frequencies for the last x moves
+        last_x_moves = "".join(opponent_play_hist[-combo_length:])
+        opponent_play_hist_combos[last_x_moves] = (
+            opponent_play_hist_combos.get(last_x_moves, 0) + 1
+        )
+        
+        predicted_play = get_next_play_prediction(
+            last_x_moves, opponent_play_hist_combos
+        )
+    else:
+        predicted_play = "R" # Default best
 
-    return guess
+    # Clear lists after each opponent
+    if len(opponent_play_hist) == 1000:
+        opponent_play_hist.clear()
+        opponent_play_hist_combos.clear()
+
+    return get_counter_move(predicted_play)
+    
+
+def get_next_play_prediction(
+    last_x_moves: str,
+    opponent_play_hist_combos: Dict[str, int] = {},
+) -> str:
+    potential_plays = [last_x_moves[1:] + move for move in "RPS"]
+    potential_play_counts = {
+        key: opponent_play_hist_combos.get(key, 0) for key in potential_plays
+    }
+    return max(potential_play_counts, key=potential_play_counts.get)[-1]
+
+
+def get_counter_move(predicted_play: str) -> str:    
+    counter_moves = {"R": "P", "P": "S", "S": "R"}
+    return counter_moves[predicted_play]
